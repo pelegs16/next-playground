9c125ee18aebed2f7d4405a2bd3b37a40b76d751
Peleg Schlider <peleg@zotapay.com>
2022-06-13 21:43:49 +0300

    fixed home

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,29 @@ const App = () => {
         <title>Home</title>
       </Head>
       <div className="p-9 border  bg-gray-50 ">
+        <div className="w-full p-5 flex flex-col space-y-4">
+          <h3>Pre Rendering</h3>
+          <p>
+            In order to see a proof for Pre-rendering - the easiest thing would be <b>to disable JavaScript</b>
+          </p>
+          <p>
+            this can be done quite fast by:
+            <ul>
+              <li>1 - entering dev tool <b>(`cmd` + `option` + `j`)</b></li>
+              <li>2 - run command <b>(`cmd` + `shift` + `p`)</b></li>
+              <li>3 - when the dialog with the <b>`Run Command`</b> appear - type
+                <span className="">{`javascript`}</span> <br/> and choose <b> <span className="">Disable Javascript</span> (orange debugger)</b></li>
+            </ul>
+
+
+          </p>
+          <p>
+            Once its done when you refresh the page - The html keep rendering.
+            <br/>
+            quite shitty. but it renders. wont happend with react
+
+          </p>
+        </div>
         <h1 className="font-bold text-xl py-2">Choose Users Page</h1>
         <div className="flex-1 flex w-full p-12 space-x-8 justify-center items-center">
           <Link href="/usersSSR">
