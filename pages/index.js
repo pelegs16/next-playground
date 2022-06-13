6b038f58f54027d91bbe8e85567ab94451028179
Peleg Schlider <peleg@zotapay.com>
2022-06-13 21:57:44 +0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Head from "next/head";
+import { useState } from "react";
 
 const App = () => {
-
+  const [showWhy, setShowWhy] = useState(false)
   return (
     <div className="min-h-screen relative p-12">
       <Head>
@@ -46,6 +47,37 @@ const App = () => {
               <a className="text-indigo-600 border border-indigo-400 p-2 rounded">Users (SSG)</a>
             </Link>
           </div>
+
+          <h4>Not Working?</h4>
+          <div>
+          <button className="text-indigo-800 border p-3 cursor-pointer" onClick={() => setShowWhy(true)}>
+            press here
+          </button>
+            <div className={`${showWhy ? 'block' : 'hidden'}  transition-all duration-300 w-full p-5  borderflex flex-col space-y-4 bg-gray-200  text-gray-800 my-3`}>
+              Thats because in dev mode static generation are pre rendered:
+              <div>
+                <blockquote className="jsx-2c4a16ef27e00414"><p>In development mode (when you run <code
+                  className="text-pink-400">npm run dev</code> or <code className="jsx-d60897d45df4a3d0">yarn
+                  dev</code>), every page is <a rel="noopener noreferrer" target="_blank"
+                                                href="/docs/basic-features/pages#pre-rendering">pre-rendered</a> on each
+                  request — even for pages that use <a rel="noopener noreferrer" target="_blank"
+                                                       href="/docs/basic-features/pages#static-generation-recommended">Static
+                    Generation</a>.</p></blockquote>
+
+                <div>
+                  enter:
+
+                  <div>
+                    https://next-playground-rust.vercel.app/
+                  </div>
+                  <div>and see in live</div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
